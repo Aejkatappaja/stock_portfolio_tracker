11a9034c12e98d249853eec9f624b506805dfa00
Aejkatappaja <joverjlj@gmail.com>
2024-09-02 18:07:39 +0200

    init(core): schema docker-compose gitignore

---
+++ schema.sql
@@ -0,0 +1,9 @@
+--CREATE THE TRANSACTIONS TABLE
+CREATE TABLE transactions (
+    id SERIAL PRIMARY KEY,
+    type VARCHAR(5) NOT NULL CHECK (char_length(type) >= 3 AND char_length(type) <= 5),
+    ticker VARCHAR(30) NOT NULL CHECK (char_length(ticker) >= 1 AND char_length(ticker) <= 30),
+    volume REAL NOT NULL,
+    price REAL NOT NULL,
+    date DATE NOT NULL
+)
